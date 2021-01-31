68492d1a5f956f1f7b56d9841f55435afcb77425
geekts25 <geekts25@gmail.com>
2021-01-31 22:57:42 +0800

    update

--- filter/filter.go
+++ filter/filter.go
@@ -1,7 +1,7 @@
 package filter
 
 import (
-	"fmt"
+	"os"
 	. "github.com/tianshi25/additionalCheck/db"
 	"github.com/tianshi25/additionalCheck/logs"
 	"github.com/go-git/go-git/v5"
@@ -9,20 +9,33 @@ import (
 
 var noPassKeys map[string]int
 
+func checkError(err error) {
+	if err == nil {
+		return
+	}
+
+	logs.E("%v occurs during git operation", err)
+	os.Exit(1)
+}
+
 func SetFilter(path string) {
 	repo, err := git.PlainOpen(path)
-	if err != nil {
-		logs.W("%v is not git repo", path)
-		return
-	}
+	checkError(err)
 
 	head, err := repo.Head()
-	if err != nil {
-		logs.E("fail to get git HEAD")
-		return
-	}
+	checkError(err)
 
-	fmt.Printf("%#v", head)
+    lastCommit, err := repo.CommitObject(head.Hash())
+	checkError(err)
+
+	logs.E("%v %v", lastCommit, lastCommit.NumParents())
+    beforeLastCommit, err := lastCommit.Parent(0)
+    checkError(err)
+
+    patch, err := lastCommit.Patch(beforeLastCommit)
+    checkError(err)
+
+    logs.E("%v", patch)
 }
 
 func filterRecords(records []Record) (ret []Record){
